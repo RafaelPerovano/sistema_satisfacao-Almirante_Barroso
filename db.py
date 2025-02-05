9d26df1c767f0ea593e7c16da5789d63c078423f
RafaelPerovano <142454508+RafaelPerovano@users.noreply.github.com>
2025-02-05 10:32:46 -0300

    alteração no db.py

--- db.py
+++ db.py
@@ -6,6 +6,10 @@ from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 load_dotenv()
 
 url = os.getenv('DATABASE_URL')
+
+if not url:
+    raise ValueError("A variável de ambiente DATABASE_URL não foi encontrada. Certifique-se de que está configurada no Render.")
+
 conn = psycopg2.connect(url)
 conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
 cursor = conn.cursor()
--- teste.py
+++ teste.py
@@ -5,8 +5,7 @@ from db import cursor, conn
 
 #cursor.execute("SELECT column_name, data_type FROM information_schema.columns WHERE table_name = 'tipo_usuario'")
 #cursor.execute("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public';")
-#cursor.execute("insert into usuarios (tipo_usuario_id, usuario) values (1, 'TESTE1.4')")
-tipo = 'Professor'
-cursor.execute("SELECT comentarios from avaliacoes where tipo_usuario = %s", (tipo,))
+cursor.execute("insert into usuarios (tipo_usuario_id, usuario) values (1, 'Karla')")
+
 mostra = cursor.fetchall()
 print(mostra)
